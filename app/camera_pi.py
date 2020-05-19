134f10c49c346b6193e119d3ffa4be85cade6a15
Justin Lam <justin@mistywest.com>
2020-05-19 14:35:29 -0700

    Reduce camera framerate to reduce bandwidth

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -3,17 +3,18 @@ import time
 import picamera
 from base_camera import BaseCamera
 
+FRAMERATE_FPS = 10
 
 class Camera(BaseCamera):
     @staticmethod
     def frames():
-        with picamera.PiCamera() as camera:
+        with picamera.PiCamera(resolution=(320,240)) as camera:
             # let camera warm up
             time.sleep(2)
 
             stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, 'jpeg',
-                                                 use_video_port=True):
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                start_time = time.time()
                 # return current frame
                 stream.seek(0)
                 yield stream.read()
@@ -21,3 +22,6 @@ class Camera(BaseCamera):
                 # reset stream for next frame
                 stream.seek(0)
                 stream.truncate()
+
+                # Wait for the target framerate
+                time.sleep(max(0, 1/FRAMERATE_FPS - (time.time() - start_time)))
